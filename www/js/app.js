1f9218c1b39fc179b1ab11ea0256f65c4b4e4711
chweeks <chrisharryweeks@gmail.com>
2015-11-02 18:47:28 +0000

    added push notifications with ionicPush

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var alarming = angular.module('alarming', ['ionic', 'ionic.service.core', 'ionic.service.push'])
 
-.run(function($ionicPlatform) {
+alarming.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +17,50 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+alarming.controller('notificationCtrl', function($http, $ionicPush) {
+
+  var self = this;
+
+  self.dataToken = '';
+
+  $ionicPush.init({
+    "debug": true,
+    "onNotification": function(notification) {
+      var payload = notification.payload;
+      console.log(notification, payload);
+    },
+    "onRegister": function(data) {
+      console.log(data.token);
+      self.dataToken = data.token;
+    }
+  });
+
+  $ionicPush.register();
+
+  self.postNotification = function() {
+    var postNotification = {
+                              "tokens":[
+                                self.dataToken
+                              ],
+                              "notification":{
+                                "alert":"Hello World!",
+                                "ios":{
+                                  "badge":1,
+                                  "sound":"ping.aiff",
+                                  "expiry": 1423238641,
+                                  "priority": 10,
+                                  "contentAvailable": 1,
+                                  "payload":{
+                                    "key1":"value",
+                                    "key2":"value"
+                                  }
+                                }
+                              }
+                            };
+    $http.post('https://push.ionic.io/api/v1/push', postNotification, 'POST').then(function successCallback(response){
+      var user_id = angular.fromJson(response).data;
+      console.log(user_id)
+   });
+  };
+});
